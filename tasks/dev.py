10bb1de47f80147d3c13f3cc1c9c2aba0fc8519a
Merijn <>
2026-08-19 19:00:00 +0200

    Move generation to the end of commands

--- tasks/dev.py
+++ tasks/dev.py
@@ -82,10 +82,10 @@ def ruff_check_cmd(fix: bool = False, exit_zero: bool = False) -> str:
 def verify(c: Context, verbose: bool = False, force: bool = False) -> None:
     """Run all code quality checks and tests"""
 
-    docs.generate_invoke_manual(c, check=True, force=force)
-    generate_api(c, check=True, force=force)
     check(c, verbose, force=force)
     test(c, verbose, force=force)
+    docs.generate_invoke_manual(c, check=True, force=force)
+    generate_api(c, check=True, force=force)
 
 
 @task(
@@ -103,11 +103,10 @@ def pre_commit(c, verbose: bool = False, force: bool = False) -> None:
     """
 
     format(c, verbose, force)
-    docs.generate_invoke_manual(c, verbose)
-    generate_api(c, verbose)
     check(c, verbose, force)
     test(c, verbose, force)
-
+    docs.generate_invoke_manual(c, verbose)
+    generate_api(c, verbose)
     if force or codebase_has_changes(BASE_DIR / "frontend"):
         setup.build_front_end.body(c, verbose=verbose, check=True)
 
