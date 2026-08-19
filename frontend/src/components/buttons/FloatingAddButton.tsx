051e467ebbfbdc8ebb29602a484bb6dbc7276cd6
Merijn <>
2026-08-19 12:11:29 +0200

    Make add button's + icon the right color
    Black in light theme
    white in dark theme

--- frontend/src/components/buttons/FloatingAddButton.tsx
+++ frontend/src/components/buttons/FloatingAddButton.tsx
@@ -1,4 +1,9 @@
+/**
+ * Floating add button fixed to the bottom-right corner.
+ */
+
 type FloatingAddButtonProps = {
+  // TODO, don't make optional
   onClick?: () => void;
   disabled?: boolean;
 };
@@ -10,21 +15,27 @@ export default function FloatingAddButton({
   return (
     <button
       type="button"
-      className="btn btn-primary rounded-circle position-fixed shadow d-flex align-items-center justify-content-center"
-      style={{
-        width: "45px",
-        height: "45px",
-        right: "24px",
-        bottom: "24px",
-        zIndex: 1050,
-        fontSize: "32px",
-        lineHeight: 1,
-      }}
+      className="
+        btn
+        btn-primary
+        rounded-circle
+        position-fixed
+        bottom-0
+        end-0
+        m-4
+        shadow
+        d-flex
+        align-items-center
+        justify-content-center
+        fs-1
+        fw-bold
+        lh-1
+      "
       onClick={onClick}
       disabled={disabled}
       aria-label="Add"
     >
-      +
+      <span className="text-body">+</span>
     </button>
   );
 }
